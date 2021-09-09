1575502bbe04f8acf12677b909cb004ba0503f91
Irina Knyazeva <iknyazeva@gmail.com>
2021-09-09 07:53:39 +0300

    resting state and task stimulation, original cole functions

--- neural_mass/nwmodel.py
+++ neural_mass/nwmodel.py
@@ -32,7 +32,7 @@ class NeuralMassModel:
         self.num_modules = num_modules
         self.syn_weight_std = syn_weight_std
         if struct_conn_probs is None:
-            self.struct_conn_probs = {'in': 0.5, 'out': 0.1}
+            self.struct_conn_probs = {'in': 0.8, 'out': 0.2}
         else:
             self.struct_conn_probs = struct_conn_probs
 
@@ -49,7 +49,7 @@ class NeuralMassModel:
         self._init_struct_matrix()
 
         if syn_com_mult is None:
-            self.syn_com_mult = {0: [1, 1, 0], 1: [0, 1, 0], 2: [0, 0, 1]}
+            self.syn_com_mult = {0: [1, 1, 0], 1: [1, 1, 0], 2: [0, 0, 1]}
         else:
             self.syn_com_mult = syn_com_mult
 
@@ -69,12 +69,97 @@ class NeuralMassModel:
         np.fill_diagonal(self.struct_matrix, 1)
 
     def _init_synaptic_weights(self):
+        #todo check synaptic
         self.synaptic_weight = self.struct_matrix * (
                     1 + np.random.standard_normal((self.num_regions, self.num_regions)) * self.syn_weight_std)
         for row in range(self.num_modules):
             for col in range(self.num_modules):
                 self.synaptic_weight[self.module_borders[row]:self.module_borders[row + 1],
                 self.module_borders[col]:self.module_borders[col + 1]] *= self.syn_com_mult[row][col]
+        np.fill_diagonal(self.synaptic_weight, 0)
+        for node in range(self.num_regions):
+            k = np.sum(self.synaptic_weight[node, :])
+            if k > 0:
+                self.synaptic_weight[node, :] = np.divide(self.synaptic_weight[node, :], k)
+
+    def _init_synpatic_cole(self):
+        struct_conn_vector = np.random.uniform(0, 1, (self.num_regions, self.num_regions)) > .90
+        # Add self-connections (important if adding autocorrelation later)
+        np.fill_diagonal(struct_conn_vector, 10)
+        self.struct_matrix = struct_conn_vector
+        # Create modular structural network (3 modules)
+        num_modules = 3
+        numr_per_module = int(round(self.num_regions / num_modules))
+        lastModuleNode = -1
+        for moduleNum in range(0, num_modules):
+            for thisNodeNum in range(lastModuleNode + 1, lastModuleNode + numr_per_module + 1):
+                # Set this node to connect to 10 random other nodes in module
+                for i in range(1, numr_per_module // 2):
+                    randNodeInModule = int(
+                        np.random.uniform(lastModuleNode + 1, lastModuleNode + numr_per_module + 1, (1, 1)))
+                    struct_conn_vector[thisNodeNum, randNodeInModule] = 1
+            lastModuleNode = lastModuleNode + numr_per_module
+
+        # Adding synaptic weights to existing structural connections (small random synapse strength variation)
+        synaptic_weight_vector = struct_conn_vector * (1 + np.random.standard_normal((self.num_regions, self.num_regions)) * .001)
+
+        # Adding synaptic mini-communities (within community 1)
+        synaptic_weight_vector[0:50, 50:100] = synaptic_weight_vector[0:50, 50:100] * -0.2
+        synaptic_weight_vector[50:100, 0:50] = synaptic_weight_vector[50:100, 0:50] * -0.2
+        synaptic_weight_vector[0:50, 0:50] = synaptic_weight_vector[0:50, 0:50] * 1.2
+        synaptic_weight_vector[50:100, 50:100] = synaptic_weight_vector[50:100, 50:100] * 1.2
+
+        # MODIFICATION: 0 connectivity between structural community 1 and 3
+        synaptic_weight_vector[0:100, 200:300] = synaptic_weight_vector[0:100, 200:300] * 0
+        synaptic_weight_vector[200:300, 0:100] = synaptic_weight_vector[200:300, 0:100] * 0
+        synaptic_weight_vector[100:200, 200:300] = synaptic_weight_vector[100:200, 200:300] * 0
+        synaptic_weight_vector[200:300, 100:200] = synaptic_weight_vector[200:300, 100:200] * 0
+
+        # Normalize each region's inputs to have a mean of 1/k, where k is the number of incoming connections (in degree)
+        # Based on Barral, J. & Reyes, A. D. Synaptic scaling rule preserves excitatory-inhibitory balance and salient neuronal network dynamics. Nat. Neurosci. (2016)
+        # This ensures that all inputs into each node sum to 1
+        for nodeNum in range(self.num_regions):
+            k = np.sum(synaptic_weight_vector[nodeNum, :])
+            if k > 0:
+                synaptic_weight_vector[nodeNum, :] = np.divide(synaptic_weight_vector[nodeNum, :], k)
+
+        self.synaptic_weight  = synaptic_weight_vector
+
+
+    def compute_network_model_cole(self, num_time_points, bias_param=-20, spont_act_level=3,
+                              g=5.0, indep=1.0, k=1):
+
+        outputdata = {'input_activity': np.zeros((num_time_points, self.num_regions)),
+                  'output_activity': np.zeros((num_time_points, self.num_regions))}
+
+        bias = np.zeros(shape=(self.num_regions,))
+        bias[range(self.num_regions)] = bias_param
+        autocorr = 0.0
+        global_coupling_mat = self.synaptic_weight*g
+        np.fill_diagonal(global_coupling_mat, 0)
+        indep_var_mat = np.identity(self.num_regions)*indep
+        self.synaptic_weight = global_coupling_mat+indep_var_mat
+        outputvect = np.zeros(self.num_regions)
+
+        for this_time_point in range(0, num_time_points):
+            stim_act_vector = np.zeros(self.num_regions)
+            if this_time_point == 0:
+                # Generate spontaneous activity for initial state
+                act_vector = sigmoid(k * (bias + np.random.normal(0, spont_act_level, (self.num_regions,))))
+                input_activity = np.zeros(self.num_regions)
+            else:
+                # Bring outputs from previous time point as inputs for this timepoint
+                act_vector = outputvect
+
+            act_vector_to_mat = np.kron(np.ones((self.num_regions, 1)), act_vector)
+            input_activity = np.multiply(autocorr, input_activity) + np.sum(
+                np.multiply(act_vector_to_mat, self.synaptic_weight), axis=1)
+
+            input_activity = input_activity + np.random.normal(0, spont_act_level, (self.num_regions,))
+            outputvect = sigmoid(k * (bias + input_activity))
+            outputdata['input_activity'][this_time_point, :] = input_activity
+            outputdata['output_activity'][this_time_point, :] = outputvect
+        return outputdata
 
     def compute_network_model(self, num_time_points, bias_param=-20, spont_act_level=3,
                               g=5.0, indep=1.0, stim_times=None, stim_t_par_start=None, stim_t_par_end=None,
